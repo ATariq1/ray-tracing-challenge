c37e499aca68dbf73df84a9b5c06d4058d20e74f
Ali <aliumar18@gmail.com>
2020-05-28 22:59:37 -0700

    Add canvas pixel get and set methods

--- src/canvas.rs
+++ src/canvas.rs
@@ -2,20 +2,70 @@
 use crate::color;
 
 struct Canvas {
-    x: i32,
-    y: i32,
-    canvas: Vec<color::Color>;
+    width : usize,
+    height: usize,
+    grid: Vec<color::Color>
 }
 
 impl Canvas {
-    pub fn new(x:i32, y:i32) -> Canvas {
 
-        
+    pub fn new(width:usize, height:usize) -> Canvas {
+        let grid = vec!( color::Color::new(0.0,0.0,0.0) ; (width*height) as usize );
+
+        Canvas {width:width, height:height, grid:grid}
     }
 
-    pub fn get_pixel(x:i32,y:i32) -> color::Color {
+    pub fn get_width(&self) -> usize {
+        self.width
+    }
 
+    pub fn get_height(&self) -> usize {
+        self.height
+    }
 
+    pub fn pixel_at(&self,x:usize,y:usize) -> color::Color {
+        
+        self.grid[self.width * y + x]
+    }
+
+    pub fn write_pixel(&mut self,x:usize,y:usize,c:color::Color) {
+
+        self.grid[self.width*y + x] = c;
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_canvas () {
+
+        let c = Canvas::new(10,20);
+        let black = color::Color::new(0.0,0.0,0.0); 
+
+        assert_eq!(10,c.get_width());
+        assert_eq!(20,c.get_height());
+        
+        for x in 0..9 {
+            for y in 0..19 {
+                assert_eq!(c.pixel_at(x,y),black);
+            }
+        }
+    }
+
+    #[test]
+    fn test_pixel_write () {
+    
+        let mut c = Canvas::new(10,20);
+        let red = color::Color::new(1.0,0.0,0.0);
+        
+        c.write_pixel(2,3,red);
+    
+        assert_eq!(c.pixel_at(2,3),red);
+
+    }
+
+
+} 
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,7 @@
-mod rt;
 mod rtx;
 mod color;
 mod canvas;
 mod projectile;
-
-
-const ITER:i32  = 25_000;
-const OUTER:u128 = 1000;
-
-use std::time::{Instant};
-
-#[allow(dead_code)]
-fn type_benchmark() {
-    let mut total = 0;
-
-    for _t in 0..OUTER { 
-    
-        let now = Instant::now();
-        let mut t = rt::vector(1.0,-1.0,10.0);
-        for x in 0..ITER {
-            let y = x as f64;
-            if rt::is_vector(t) {
-                t = rt::cdiv(2.0,rt::add(t,rt::cmul(y,t)));
-            }
-        }
-
-        total += now.elapsed().as_nanos();
-    }
-
-    println!("TUPLE AVG : {}",total/OUTER);
-
-    let mut total = 0;
-
-    for _t in 0..OUTER {
-    
-        let now = Instant::now();
-        let mut t = rtx::Geo::vector(1.0,-1.0,10.0);
-        for x in 0..ITER {
-            let y = x as f64;
-            if t.is_vector() {
-                t = (t + y*t)/2.0;
-            }
-        }
-        total += now.elapsed().as_nanos();
-    }
-
-    println!("STRUCT AVG: {}",total/OUTER);
-
-}
-
-
 
 fn main() {
 
